914543ba97273d7c31ea25523f9402ba7585903b
huyunsong <huyunsong@58.com>
2021-07-27 15:00:18 +0800

    创建一个无限滚动的 ListView

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 //引入material库
 import 'package:flutter/material.dart';
+
 //引入english_word
 import 'package:english_words/english_words.dart';
 
@@ -17,14 +18,7 @@ class MyApp extends StatelessWidget {
       title: "First Demo",
       // Scaffold 是 Material 库中提供的一个 widget，
       // 它提供了默认的导航栏、标题和包含主屏幕 widget 树的 body 属性。
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text("First Demo"),
-        ),
-        body:  Center(
-          child: RandomWords(),
-        ),
-      ),
+      home: RandomWords(),
     );
   }
 }
@@ -45,9 +39,48 @@ class RandomWords extends StatefulWidget {
 }
 
 class _RandomWordsState extends State<RandomWords> {
+  /// 保存建议的单词对
+  final _suggestions = <WordPair>[];
+
+  /// 增大字体大小
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
-    return Text(wordPair.asPascalCase);
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("StateFul Widget"),
+      ),
+      body: _buildSuggestions(),
+    );
+  }
+
+  Widget _buildSuggestions() {
+    return ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        // 对于每个建议的单词对都会调用一次 itemBuilder
+        itemBuilder: (context, i) {
+          // 在奇数行，该函数会添加一个分割线的 widget，来分隔相邻的词对。
+          if (i.isOdd) {
+            return const Divider();
+          }
+          // 语法 i ~/ 2 表示 i 除以 2，但返回值是整型（向下取整）,
+          // 可以计算出 ListView 中减去分隔线后的实际单词对数量。
+          final index = i ~/ 2;
+          // 如果是建议列表中最后一个单词对，接着再生成10个单词对，然后添加到建议列表。
+          if (index >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs().take(10));
+          }
+          return _buildRow(_suggestions[index]);
+        });
+  }
+
+  Widget _buildRow(WordPair pair) {
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+    );
   }
 }
