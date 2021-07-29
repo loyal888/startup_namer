bd46250ca9033151b72d3eb3073df5efea070d5d
huyunsong <huyunsong@58.com>
2021-07-29 10:25:28 +0800

    shared_preferences的使用

---
+++ lib/SharedPreferenceExample.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+import 'package:shared_preferences/shared_preferences.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: ElevatedButton(
+            onPressed: _incrementCounter,
+            child: Text('Increment Counter'),
+          ),
+        ),
+      ),
+    ),
+  );
+}
+
+/// shared_preferences的使用
+void _incrementCounter() async {
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  int counter = (prefs.getInt('counter') ?? 0) + 1;
+  print('Pressed $counter times.');
+  prefs.setInt('counter', counter);
+}
